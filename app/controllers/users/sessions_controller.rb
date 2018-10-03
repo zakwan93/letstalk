5866c65b96c9958c3d1440522b1d9c4eb27fb937
zakwan93 <zakwan_bhaiyat@yahoo.com>
2018-10-03 18:18:11 -0400

    changed and add more stuff in registration and session in USER mvc

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,10 +2,10 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
+  layout false
   # GET /resource/sign_in
   # def new
-  #   super
+  #   super 
   # end
 
   # POST /resource/sign_in
